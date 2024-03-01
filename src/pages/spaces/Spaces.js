90c978b8258ea699f3a07d894980861968c7a310
AustinLian <75471463+Austinliannn@users.noreply.github.com>
2024-03-01 15:19:08 +0800

    Header component
    Created header component with sass

--- src/pages/spaces/Spaces.js
+++ src/pages/spaces/Spaces.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Header from "../../components/Header";
 
 function Spaces() {
-return (
+  return (
     <>
+      <Header />
     </>
-    );
+  );
 }
 
 export default Spaces;
-    
