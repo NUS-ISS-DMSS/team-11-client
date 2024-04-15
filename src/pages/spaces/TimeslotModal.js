9ae2057a5483a338d32f83ea338cc8d651f45756
AustinLian <75471463+Austinliannn@users.noreply.github.com>
2024-04-15 16:51:16 +0800

    Update reservation builder

--- src/pages/spaces/TimeslotModal.js
+++ src/pages/spaces/TimeslotModal.js
@@ -3,6 +3,7 @@ import { Modal, Row, Col, Button, Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { createTimeSlots } from "../../api/timeSlotsApi";
 import { createReservation } from "../../api/reservationsApi";
+import ReservationDataBuilder from "./ReservationBuilder";
 
 export default function TimeslotModal(props) {
   const navigate = useNavigate();
@@ -84,42 +85,6 @@ export default function TimeslotModal(props) {
       setWarningMessage("Please select a date and timeslot");
     }
   };
-
-  // ReservationDataBuilder class for constructing reservation data
-  class ReservationDataBuilder {
-    constructor() {
-      this.data = {};
-    }
-
-    setReservationDate(date) {
-      this.data.reservation_date = date;
-      return this;
-    }
-
-    setSpaceId(spaceId) {
-      this.data.space = { id: spaceId };
-      return this;
-    }
-
-    setUserId(userId) {
-      this.data.user = { id: userId };
-      return this;
-    }
-
-    setTimeSlotId(timeSlotId) {
-      this.data.timeSlot = { id: timeSlotId };
-      return this;
-    }
-
-    setStatus(status) {
-      this.data.status = status;
-      return this;
-    }
-
-    build() {
-      return this.data;
-    }
-  }
 
   const handleSubmit = async (e) => {
     if (!checkboxQuery || !selectedDate) return;
